e1da485c226dda6b9a5c408d65d4082982e6633f
iamsadat <sadatullahkhan40@gmail.com>
2023-07-11 17:40:07 +0530

    Fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,14 +35,13 @@ const Footer = () => {
         <div className="footer__col">
           <h4 className="footer__col-title">Location</h4>
           <iframe
-            title="Farm Location"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3807.3051644107827!2d78.40156509280706!3d17.397136722812256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb9697426fa711%3A0x804ba10ecbcdd1f5!2sQutb%20Shahi%20Tombs!5e0!3m2!1sen!2sin!4v1688826528457!5m2!1sen!2sin"
-            width="100%"
-            height="200"
-            style={{ border: 0 }}
-            allowFullScreen=""
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3810.102973825073!2d78.3255059!3d17.262252399999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcbbf271a7aa60b%3A0x1fee920eb7355b98!2sA%20Creations%20Farm%20Stay!5e0!3m2!1sen!2sin!4v1689019312797!5m2!1sen!2sin"
+            width="400"
+            height="300"
+            style={{ border: "0" }}
+            allowfullscreen=""
             loading="lazy"
-            className="footer__map"
+            referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
       </div>
